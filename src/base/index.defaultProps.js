65babcd2aa2599a8c957ea5f63dfbf99351901c9
SirYummy <micahbole.n@gmail.com>
2018-02-07 11:53:51 -0600

    chore: add default props

---
+++ src/base/index.defaultProps.js
@@ -0,0 +1,11 @@
+import CREATOR from './index.props.creator'
+import COLORS from './index.props.colors'
+import TIME from './index.props.time'
+import DELTAS from './index.props.deltas'
+
+export default {
+	CREATOR,
+	COLORS,
+	TIME,
+	DELTAS
+}
